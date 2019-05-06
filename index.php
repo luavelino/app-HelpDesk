d8d0ba3f4e248b7a49f312afbdf430be93494b19
luavelino <30575432+luavelino@users.noreply.github.com>
2019-05-05 23:42:13 -0300

    Front End do app Help Desk

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<nav class="navegacao">
+			<a href="#">Logo</a>
+		</nav>
+		<div class="container">
+			<form action="GET" method="">
+				<input type="text" name="login" placeholder="Login">
+				<input type="password" name="senha" placeholder="Senha">
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	</body>
+</html>
